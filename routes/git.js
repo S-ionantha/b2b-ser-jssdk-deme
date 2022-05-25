0a70fce230d95a40aca878d4f1054b39dd1ef7b4
ionantha <sunziwen@baidu.com>
2022-05-25 11:25:36 +0800

    git

--- routes/git.js
+++ routes/git.js
@@ -1,13 +1,13 @@
 /**
- * @file: 
+ * @file:
  * @author: sunziwen(sunziwen@baidu.com)
  */
- var express = require('express');
- var router = express.Router();
- 
- router.post('/WebHook', function(req, res, next) {
-   res.render('index', { title: 'Express' });
- });
- 
- module.exports = router;
- 
+var express = require('express');
+var router = express.Router();
+
+router.post('/WebHook', function (req, res, next) {
+    console.log(req.body);
+    res.json({status: 200, statusText: 'success'});
+});
+
+module.exports = router;
