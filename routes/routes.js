4672221f2349bb5b22435876c2652afebfca2017
ionantha <sunziwen@baidu.com>
2022-05-25 11:54:55 +0800

    fix

---
+++ routes/routes.js
@@ -0,0 +1,33 @@
+/**
+ * @file: 自动配置路由
+ * @author: sunziwen(sunziwen@baidu.com)
+ */
+
+import fs from 'fs';
+
+let list = [];
+let PATH = './routes';
+const reg = new RegExp('.js$', '');
+
+let routerObj = (file, basePath, routerPath) => {
+    return {
+        path: file === 'index' ? basePath : routerPath,
+        router: require('.' + routerPath)
+    };
+};
+
+const setRouter = (filePath, basePath, routerPath) => {
+    let files = fs.readdirSync(filePath);
+    files.forEach(file => {
+        if (reg.test(file) && file !== 'routes.js') {
+            file = file.replace(/\.js$/, '');
+            list.push(routerObj(file, basePath, routerPath + file));
+        }
+        else if (!reg.test(file)) {
+            setRouter(filePath + file, basePath + file, routerPath + file);
+        }
+    });
+};
+setRouter(PATH, '/', '/');
+
+export default list;
