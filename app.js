a2efd2b989eb289afba02a0ec44a4e3a41accd28
ionantha <sunziwen@baidu.com>
2022-05-26 14:50:06 +0800

    fix

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ var API = require('./routes/routes');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // 注册路由
 API.forEach(item => {
---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        编辑功能
+        <div
+            ref="code"
+            class="code"
+        ></div>
+
+        <div
+            ref="res"
+            class="code"
+        ></div>
+
+    </div>
+</template>
+
+<script>
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+export default {
+    name: "App",
+    data() {
+        return {
+            editor: null,
+            code: `window.acgAppSdk.refreshTicket(\n\tnull,\n\t{\n\t\tsuccess: (data) => {\n\t\t\tconsole.log(data)\n\t\t}\n\t}\n)\nconsole.log(123)`,
+        };
+    },
+    mounted() {
+        this.editor = monaco.editor.create(this.$refs.code, {
+            value: this.code,
+            language: "javascript",
+            lineNumbers: false,
+            lineHeight: 20, // 行高
+            tabSize: 4, // 缩进
+            overviewRulerBorder: false,
+            overviewRulerLanes: false,
+            theme: "vs-dark",
+            minimap: {
+                // 关闭代码缩略图
+                enabled: false, // 是否启⽤预览图
+            },
+            fontSize: "14px",
+            autoIndent: true,
+        });
+        // let data = eval(this.code);
+        // console.log(this.editor.getValue());
+
+        // this.$refs.res.innerHTML = this.editor.getValue()
+    },
+    method: {},
+};
+</script>
+
+<style>
+.code {
+    border: 1px solid black;
+    width: 500px;
+    height: 300px;
+    white-space: pre-wrap;
+}
+</style>
