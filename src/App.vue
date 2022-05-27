a553324d739bc0b834fd4f9feb98e5eafe52ebde
ionantha <sunziwen@baidu.com>
2022-05-27 13:37:56 +0800

    f

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,29 @@
     <div>
         编辑功能
         <div class="code-area">
-            <div
+            <!-- <div
                 ref="code"
                 class="code"
-            ></div>
+            ></div> -->
+            <MonacoEditor
+                height="300"
+                width="500"
+                class="vs"
+                style="text-align: left;background-color: #fff"
+                language="javascript"
+                :code="code"
+                :editorOptions="options"
+                @mounted="onMounted"
+                @codeChange="onCodeChange"
+            >
+            </MonacoEditor>
             <el-button
                 type="primary"
                 @click="runCode"
             >运行</el-button>
             <div
                 ref="res"
-                class="res"
+                class="res code"
             ></div>
         </div>
 
@@ -20,43 +32,83 @@
 </template>
 
 <script>
-import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import MonacoEditor from "vue-monaco-editor";
 export default {
     name: "App",
+    components: {
+        MonacoEditor,
+    },
     data() {
         return {
             editor: null,
-            code: `window.acgAppSdk.refreshTicket(\n\tnull,\n\t{\n\t\tsuccess: (data) => {\n\t\t\tconsole.log(data)\n\t\t}\n\t}\n)\nconsole.log(123)`,
+            code: '', //"// 用于存储输出值\nlet print;\n\n",
+            options: {
+                theme: "vs",
+                selectOnLineNumbers: false,
+                lineNumbers: "off",
+                roundedSelection: false,
+                readOnly: false,
+                automaticLayout: true,
+                glyphMargin: true,
+                showFoldingControls: "always",
+                formatOnPaste: true,
+                formatOnType: true,
+                folding: true,
+            },
+            refreshTicket: [
+                "function success (data) {",
+                "    print(data);",
+                "}",
+                "function refreshTicket() {",
+                "    window.acgAppSdk.refreshTicket(",
+                "        null,",
+                "        {",
+                "            success: success",
+                "        }",
+                "    )",
+                "}",
+                "refreshTicket();",
+            ],
         };
     },
     mounted() {
-        this.editor = monaco.editor.create(this.$refs.code, {
-            value: this.code,
-            language: "javascript",
-            lineNumbers: false,
-            lineHeight: 20, // 行高
-            tabSize: 4, // 缩进
-            overviewRulerBorder: false,
-            overviewRulerLanes: false,
-            theme: "vs-dark",
-            minimap: {
-                // 关闭代码缩略图
-                enabled: false, // 是否启⽤预览图
-            },
-            fontSize: "14px",
-            autoIndent: true,
-        });
+        // this.editor = monaco.editor.create(this.$refs.code, {
+        //     value: this.code,
+        //     language: "javascript",
+        //     lineNumbers: false,
+        //     lineHeight: 20, // 行高
+        //     tabSize: 4, // 缩进
+        //     overviewRulerBorder: false,
+        //     overviewRulerLanes: false,
+        //     theme: "vs-dark",
+        //     minimap: {
+        //         // 关闭代码缩略图
+        //         enabled: false, // 是否启⽤预览图
+        //     },
+        //     fontSize: "14px",
+        //     autoIndent: true,
+        // });
         // let data = eval(this.code);
         // console.log(this.editor.getValue());
-
         // this.$refs.res.innerHTML = this.editor.getValue()
+        window.print = (data) => {
+            this.$refs.res.innerHTML = data;
+        }
     },
     methods: {
         runCode() {
-            console.log('this.editor.getValue()');
-            // console.log(this.editor.getValue());
-            console.log(this.$refs.code.innerText);
-            console.log(eval(this.$refs.code.innerText))
+            let newCode = `${this.editor.getValue()}`;
+            let res = eval(newCode);
+            console.log(res);
+            // this.$refs.res.innerHTML = res;
+        },
+        onMounted(editor) {
+            this.editor = editor;
+            this.editor.setValue(this.refreshTicket.join('\n'))
+        },
+
+        onCodeChange(editor) {
+            console.log(editor.getValue());
         },
     },
 };
@@ -65,6 +117,7 @@ export default {
 <style>
 .code-area {
     display: flex;
+    justify-content: space-around;
 }
 .code {
     border: 1px solid black;
