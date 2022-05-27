f6bec61a747143fbca79f18a12f54def0efea742
ionantha <sunziwen@baidu.com>
2022-05-27 17:30:56 +0800

    fix

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,7 @@ router.get('/list', async (req, res) => {
         
         access_token = accessData.access_token
     }
-
+    console.log('offline')
     let {data: list} = await axios.get(
         `https://openapi.baidu.com/rest/2.0/b2b_tp/offline/goods/list?app_id=172&access_token=${access_token}&ticket=${query.ticket}`
     );
