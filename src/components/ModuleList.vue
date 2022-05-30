2aa2d94981bb6bc29d876ed75c8caff90a6a3c67
ionantha <sunziwen@baidu.com>
2022-05-30 17:09:21 +0800

    f

--- src/components/ModuleList.vue
+++ src/components/ModuleList.vue
@@ -129,14 +129,13 @@ h3 {
 .name {
     font-weight: 500;
     margin-top: 4px;
-    /* ellipsis-line(2) */
     line-height: 22px;
     font-size: 17px;
     color: #1f1f1f;
-    overflow hidden
-    text-overflow ellipsis
-    display -webkit-box
-    -webkit-line-clamp line
-    -webkit-box-orient vertical
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2
+    -webkit-box-orient: vertical;
 }
 </style>
