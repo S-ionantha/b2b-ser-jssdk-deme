d628b3d5ba2b61b93e539256fdd472e16a1ca5b6
ionantha <sunziwen@baidu.com>
2022-05-30 17:15:19 +0800

    f

--- src/components/ModuleList.vue
+++ src/components/ModuleList.vue
@@ -141,7 +141,7 @@ h3 {
     overflow: hidden;
     text-overflow: ellipsis;
     display: -webkit-box;
-    -webkit-line-clamp: 2
+    -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
 }
 </style>
